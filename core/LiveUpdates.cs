c7994fcb5879ad1c1b4ef8d306049ac9a2185b71
Haï~ <hai-vr@users.noreply.github.com>
2025-09-05 06:27:27 +0200

    Add data structures for live status updates: - Prepare data structure for live status updates backend.

---
+++ core/LiveUpdates.cs
@@ -0,0 +1,59 @@
+﻿namespace XYVR.Core;
+
+public class LiveUpdate
+{
+    public NamedApp namedApp;
+    public string qualifiedAppName;
+    public string inAppIdentifier;
+
+    public OnlineStatus? onlineStatus;
+    public LiveKnownSession? mainSession;
+}
+
+public enum OnlineStatus
+{
+    Offline,
+    Online,
+    // Resonite
+    Busy,
+    Away,
+    Sociable,
+    // VRChat
+    AskMe,
+    DoNotDisturb,
+    JoinMe
+}
+
+public class LiveSessionState
+{
+    public LiveKnownSession? knownSession;
+    public LiveSessionKnowledge knowledge;
+}
+
+public enum LiveSessionKnowledge
+{
+    Indeterminate,
+    Known,
+    // Resonite
+    ContactsOnlyWorld,
+    PrivateSession,
+    // VRChat
+    PrivateWorld,
+    OffPlatform,
+}
+
+public class LiveKnownSession
+{
+    public string inAppSessionIdentifier;
+    public string? inAppSessionName;
+    
+    public string inAppVirtualSpaceName;
+    
+    public LiveSessionHost? inAppHost;
+}
+
+public class LiveSessionHost
+{
+    public string inAppHostIdentifier;
+    public string? inAppHostDisplayName;
+}
