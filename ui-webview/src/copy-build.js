03fd6989889d448895cf6e1ee790b8723394833a
Haï~ <hai-vr@users.noreply.github.com>
2025-09-16 02:47:31 +0200

    Fix don't use xcopy on Linux during npm run build-and-copy: - Migrate away from xcopy so that the script may also run on Linux. - Also, don't copy to ui-webview when the OS is not Windows.

---
+++ ui-webview/src/copy-build.js
@@ -0,0 +1,49 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+
+/**
+ * Recursively copies a directory from source to destination
+ */
+function copyDir(src, dest) {
+    if (!fs.existsSync(dest)) {
+        fs.mkdirSync(dest, { recursive: true });
+    }
+
+    fs.readdirSync(src).forEach(item => {
+        const srcPath = path.join(src, item);
+        const destPath = path.join(dest, item);
+
+        if (fs.statSync(srcPath).isDirectory()) {
+            copyDir(srcPath, destPath);
+        } else {
+            fs.copyFileSync(srcPath, destPath);
+        }
+    });
+}
+
+const sourceDir = 'dist';
+const allDestinations = [
+    '../bin/Debug/net9.0-windows/src/dist',
+    '../../ui-photino/bin/Debug/net9.0/wwwroot',
+    '../../ui-photino/wwwroot'
+];
+
+const destinations = os.platform() === 'win32'
+    ? allDestinations
+    : allDestinations.filter(dest => !dest.includes('net9.0-windows'));
+
+console.log('Starting file copy process...');
+
+destinations.forEach(dest => {
+    try {
+        console.log(`Copying ${sourceDir} to ${dest}...`);
+        copyDir(sourceDir, dest);
+        console.log(`✓ Successfully copied to ${dest}`);
+    } catch (error) {
+        console.error(`✗ Failed to copy to ${dest}:`, error.message);
+        process.exit(1);
+    }
+});
+
+console.log('All files copied successfully!');
