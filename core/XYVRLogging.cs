40b9689819bce57ebfa71f8d1ef59725df8cc16d
Haï~ <hai-vr@users.noreply.github.com>
2025-09-21 03:53:37 +0200

    Switch back to console logs: - Diagnostic logs are too annoying for the user to report, with the current logging system we have in place. Use Console logs for now until we have a better solution.

--- core/XYVRLogging.cs
+++ core/XYVRLogging.cs
@@ -4,16 +4,16 @@ public static class XYVRLogging
 {
     public static void WriteLine(string str)
     {
-        System.Diagnostics.Debug.WriteLine(str);
+        Console.WriteLine(str);
     }
     
     public static void WriteLine(Exception e)
     {
-        System.Diagnostics.Trace.TraceError("Exception occurred: {0}\nStack Trace: {1}", e.Message, e.StackTrace);
+        Console.Error.WriteLine("Exception occurred: {0}\nStack Trace: {1}", e.Message, e.StackTrace);
     }
 
     public static void ErrorWriteLine(string s)
     {
-        System.Diagnostics.Trace.TraceError(s);
+        Console.Error.WriteLine(s);
     }
 }
