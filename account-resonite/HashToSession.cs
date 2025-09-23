754bbc52b3d25b9f6b3138d4cd7d83758f99b445
Haï~ <hai-vr@users.noreply.github.com>
2025-09-23 14:24:15 +0200

    Try to fix sessionIds were incorrectly modified during enumeration: - Use a lock to prevent a crash caused by sessionIds being modified while enumerating it.

--- account-resonite/HashToSession.cs
+++ account-resonite/HashToSession.cs
@@ -6,26 +6,62 @@ internal class HashToSession
     private readonly HashSet<string> _sessionIds = new();
     private readonly Dictionary<string, SessionBrief> _sessionIdToSessionBrief = new();
     private readonly Dictionary<string, string> _mixedHashesToSessionId = new();
+    private readonly ReaderWriterLockSlim _lock = new();
 
     public void SubmitSession(SessionBrief brief)
     {
-        _sessionIds.Add(brief.sessionId);
-        _sessionIdToSessionBrief.TryAdd(brief.sessionId, brief);
+        _lock.EnterWriteLock();
+        try
+        {
+            _sessionIds.Add(brief.sessionId);
+            _sessionIdToSessionBrief.TryAdd(brief.sessionId, brief);
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
     }
 
     public async Task<SessionBrief?> ResolveSession(string wantedHash, string salt)
     {
-        if (_mixedHashesToSessionId.TryGetValue(wantedHash, out var value))
+        _lock.EnterReadLock();
+        try
         {
-            return _sessionIdToSessionBrief[value];
+            if (_mixedHashesToSessionId.TryGetValue(wantedHash, out var value))
+            {
+                return _sessionIdToSessionBrief[value];
+            }
         }
-        
-        foreach (var sessionId in _sessionIds)
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+
+        _lock.EnterReadLock();
+        string[] sessionIdsCopy;
+        try
+        {
+            sessionIdsCopy = _sessionIds.ToArray();
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+
+        foreach (var sessionId in sessionIdsCopy)
         {
             if (await ResoniteHash.Rehash(sessionId, salt) == wantedHash)
             {
-                _mixedHashesToSessionId.TryAdd(wantedHash, sessionId);
-                return _sessionIdToSessionBrief[sessionId];
+                _lock.EnterWriteLock();
+                try
+                {
+                    _mixedHashesToSessionId.TryAdd(wantedHash, sessionId);
+                    return _sessionIdToSessionBrief.GetValueOrDefault(sessionId);
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
             }
         }
 
