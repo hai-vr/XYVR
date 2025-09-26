2f4cb895bac9b563c9a7daf8416cbc12c5d55998
Haï~ <hai-vr@users.noreply.github.com>
2025-09-26 09:24:17 +0200

    Delete cached worlds not seen for more than 45 days: - To prevent RAM usage from increasing indefinitely, delete cached worlds that haven't been seen for more than 45 days.

--- core/WorldNameCache.cs
+++ core/WorldNameCache.cs
@@ -3,15 +3,20 @@
 public class WorldNameCache
 {
     private const int LatestVersion = 3;
-    
+    private const string Warning = "THE CONTENTS OF THIS FILE CAN BE DELETED AT ANY TIME. You must NOT use this file as a world data archival source; it is not the purpose of this file.";
+    private const string Purpose = "The purpose of this file is to prevent repetitive requests to the VRChat API to show the world name of active sessions when restarting the application.";
+
     public int cacheVersion = LatestVersion;
-    public string warning = "THE CONTENTS OF THIS FILE CAN BE DELETED AT ANY TIME. You must NOT use this file as a world data archival source; it is not the purpose of this file.";
-    public string purpose = "The purpose of this file is to prevent repetitive requests to the VRChat API to show the world name of active sessions when restarting the application.";
+    public string warning = Warning;
+    public string purpose = Purpose;
     
     public Dictionary<string, CachedWorld> VRCWorlds = new();
 
     public void PreProcess()
     {
+        warning = Warning;
+        purpose = Purpose;
+        
         if (cacheVersion != LatestVersion)
         {
             foreach (var value in VRCWorlds.Values)
@@ -24,13 +29,28 @@ public class WorldNameCache
         }
 
         var now = DateTime.Now;
-        foreach (var value in VRCWorlds.Values)
+
+        // Delete entries from the world cache older than 45 days. This is because the world cache could grow indefinitely,
+        // and we are loading the data into RAM, so we don't want unnecessary data to be loaded to RAM every time.
+        // The value in "cachedAt" is refreshed along with the data when someone is witnessed in that world after 6 hours.
+        var keys = VRCWorlds.Keys.ToList();
+        foreach (var key in keys)
         {
-            if (!value.isObsolete)
+            var world = VRCWorlds[key];
+            if ((now - world.cachedAt).Duration().TotalDays > 45)
             {
-                if ((now - value.cachedAt).Duration().TotalHours > 6)
+                VRCWorlds.Remove(key);
+            }
+        }
+        
+        foreach (var world in VRCWorlds.Values)
+        {
+            if (!world.isObsolete)
+            {
+                // We refresh the data in case the world name, thumbnail URL, or capacity has changed.
+                if ((now - world.cachedAt).Duration().TotalHours > 6)
                 {
-                    value.needsRefresh = true;
+                    world.needsRefresh = true;
                 }
             }
         }
